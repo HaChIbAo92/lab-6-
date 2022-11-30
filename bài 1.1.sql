25bc10926feb7c1196365b11f4e6dd89f4293220
HaChIbAo92 <118881798+HaChIbAo92@users.noreply.github.com>
2022-11-30 20:41:13 +0700

    Add files via upload

---
+++ bài 1.1.sql
@@ -0,0 +1,11 @@
+﻿create trigger trg_TMNV on NhanVien
+for insert
+as 
+     if(select luong from inserted)<15000
+	    begin
+		  print 'Luong phai lon hon 15000'
+		  rollback transaction
+		end
+
+insert into NHANVIEN
+values ('HÀ', 'CHÍ', N'BẢO', '1725', '2002-10-17', 'TPHCM', 'Nam', 30000, '004',1)
