e5e4676caf4c8350923d55223d8fc9f0e07d7d8f
HaChIbAo92 <118881798+HaChIbAo92@users.noreply.github.com>
2022-11-30 22:33:24 +0700

    Add files via upload

---
+++ bai 3.2.sql
@@ -0,0 +1,7 @@
+﻿create trigger nhanvien3 on NHANVIEN
+after insert 
+as
+begin
+insert into PHANCONG values ((select manv from inserted), 1,2,20)
+end
+INSERT INTO NHANVIEN VALUES ('sfd','Xu','Hi','031','7-12-1999','Hà nội','Nam',60000,'003',1)
